318e499160e869d47826f7b7074bc28852466975
xcitic <xcitic@gmail.com>
2020-11-05 23:21:56 +0100

    Nav back icon

--- frontends/app/src/components/NavBar.vue
+++ frontends/app/src/components/NavBar.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="navbar">
         <Settings v-if="mainPage"></Settings>
-        <div v-else @click="goBack">GoBack</div>
+        <div v-else @click="goBack">
+            <svg style="width:4rem;height:4rem" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+            </svg>
+        </div>
     </div>
 </template>
 
